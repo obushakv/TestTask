e8506659ed958af6b827a495ae370104a4f3d690
Obushak Vasyl <obushakv@gmail.com>
2023-06-07 23:42:37 +0300

    Updated target framework to .Net 6. Updated oudated (ConfigurationManager) and vulnerable (SqlClient) packages Updated namespaces syntax

--- LegacyApp/ClientRepository.cs
+++ LegacyApp/ClientRepository.cs
@@ -2,41 +2,40 @@
 using System.Data;
 using System.Data.SqlClient;
 
-namespace LegacyApp
+namespace LegacyApp;
+
+public class ClientRepository
 {
-    public class ClientRepository
+    public Client GetById(int id)
     {
-        public Client GetById(int id)
+        Client client = null;
+        var connectionString = ConfigurationManager.ConnectionStrings["appDatabase"].ConnectionString;
+
+        using (var connection = new SqlConnection(connectionString))
         {
-            Client client = null;
-            var connectionString = ConfigurationManager.ConnectionStrings["appDatabase"].ConnectionString;
-
-            using (var connection = new SqlConnection(connectionString))
+            var command = new SqlCommand
             {
-                var command = new SqlCommand
+                Connection = connection,
+                CommandType = CommandType.StoredProcedure,
+                CommandText = "uspGetClientById"
+            };
+
+            var parametr = new SqlParameter("@clientId", SqlDbType.Int) { Value = id };
+            command.Parameters.Add(parametr);
+            
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            while (reader.Read())
+            {
+                client = new Client
                 {
-                    Connection = connection,
-                    CommandType = CommandType.StoredProcedure,
-                    CommandText = "uspGetClientById"
+                    Id = int.Parse(reader["ClientId"].ToString()),
+                    Name = reader["Name"].ToString(),
+                    ClientStatus = (ClientStatus) int.Parse(reader["ClientStatus"].ToString())
                 };
-
-                var parametr = new SqlParameter("@clientId", SqlDbType.Int) { Value = id };
-                command.Parameters.Add(parametr);
-                
-                connection.Open();
-                var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
-                while (reader.Read())
-                {
-                    client = new Client
-                    {
-                        Id = int.Parse(reader["ClientId"].ToString()),
-                        Name = reader["Name"].ToString(),
-                        ClientStatus = (ClientStatus) int.Parse(reader["ClientStatus"].ToString())
-                    };
-                }
             }
-
-            return client;
         }
+
+        return client;
     }
 }
--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
-namespace LegacyApp
+namespace LegacyApp;
+
+public class User
 {
-    public class User
-    {
-        public Client Client { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string EmailAddress { get; set; }
-        public string Firstname { get; set; }
-        public string Surname { get; set; }
-        public bool HasCreditLimit { get; set; }
-        public int CreditLimit { get; set; }
-    }
+    public Client Client { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string EmailAddress { get; set; }
+    public string Firstname { get; set; }
+    public string Surname { get; set; }
+    public bool HasCreditLimit { get; set; }
+    public int CreditLimit { get; set; }
 }
